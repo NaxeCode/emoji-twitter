a4d80fd82f2b9d1f7d8816b29c8af4295d22d3ba
NaxeCode <siraj.n.lee@gmail.com>
2023-07-12 10:11:35 -0700

    added in a CreatePostWizard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,27 @@ import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
 
+const CreatePostWizard = () => {
+  const { user } = useUser();
+
+  if (!user) return null;
+
+  return (
+    <div className="flex gap-3 w-full">
+      <img src={user.profileImageUrl} alt="Profile Image" className="w-14 h-14 rounded-full" />
+      <input placeholder="Type some emojis!" className="grow bg-transparent outline-none" />
+    </div>
+  );
+};
+
 export default function Home() {
   const user = useUser();
 
   const { data, isLoading } = api.posts.getAll.useQuery();
 
-  
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <div>Loading...</div>;
 
-  if (!data) return <div>Something went wrong</div>
+  if (!data) return <div>Something went wrong</div>;
 
   return (
     <>
@@ -22,14 +34,20 @@ export default function Home() {
       </Head>
       <main className="flex h-screen justify-center">
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
-          <div className="border-b border-slate-400 p-4 flex">
-            {!user.isSignedIn && <div className="flex justify-center"><SignInButton /></div>}
-            {!!user.isSignedIn && <SignOutButton />}
+          <div className="flex border-b border-slate-400 p-4">
+            {!user.isSignedIn && (
+              <div className="flex justify-center">
+                <SignInButton />
+              </div>
+            )}
+            {user.isSignedIn && <CreatePostWizard />}
           </div>
           <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
           <div className="flex flex-col">
             {[...data, ...data]?.map((post) => (
-              <div key={post.id} className="p-8 border-b border-slate-400">{post.content}</div>
+              <div key={post.id} className="border-b border-slate-400 p-8">
+                {post.content}
+              </div>
             ))}
           </div>
         </div>
