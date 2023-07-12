49a4bdbd9ce564a9811013cd3ecf893dab1d98e4
NaxeCode <siraj.n.lee@gmail.com>
2023-07-12 11:04:16 -0700

    Implemented user images on posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,37 @@
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 import Link from "next/link";
-import { api } from "~/utils/api";
+import { RouterOutputs, api } from "~/utils/api";
 
 const CreatePostWizard = () => {
   const { user } = useUser();
 
+  console.log(user);
+
   if (!user) return null;
 
   return (
-    <div className="flex gap-3 w-full">
-      <img src={user.profileImageUrl} alt="Profile Image" className="w-14 h-14 rounded-full" />
-      <input placeholder="Type some emojis!" className="grow bg-transparent outline-none" />
+    <div className="flex w-full gap-3">
+      <img
+        src={user.profileImageUrl}
+        alt="Profile Image"
+        className="h-14 w-14 rounded-full"
+      />
+      <input
+        placeholder="Type some emojis!"
+        className="grow bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div key={post.id} className="border-b border-slate-400 p-8">
+      <img src={author.profileImageUrl} />
+      {post.content}
     </div>
   );
 };
@@ -44,10 +64,8 @@ export default function Home() {
           </div>
           <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
           <div className="flex flex-col">
-            {[...data, ...data]?.map((post) => (
-              <div key={post.id} className="border-b border-slate-400 p-8">
-                {post.content}
-              </div>
+            {[...data, ...data]?.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
             ))}
           </div>
         </div>
