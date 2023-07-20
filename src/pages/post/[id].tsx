1939617b5f501f3814e22f7a8bc2c423a7a05ee6
NaxeCode <siraj.n.lee@gmail.com>
2023-07-20 06:21:30 -0700

    Full Post page added

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,14 +1,45 @@
 import Head from "next/head";
+import { api } from "~/utils/api";
+import type { GetServerSidePropsContext } from "next";
+import { PageLayout } from "~/components/layout";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+import { PostView } from "~/components/postview";
 
-export default function SinglePostPage() {
+export default function SinglePostPage({ id }: { id: string }) {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
+
+  //console.log(username);
+
+  if (!data) return <div>404</div>;
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>Single Post View</div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
+}
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{ id: string }>
+) {
+  const helpers = generateSSGHelper();
+  const id = context.params?.id;
+
+  if (typeof id !== "string") throw new Error("No Slug!");
+
+  await helpers.posts.getById.prefetch({ id });
+  //await helpers.post.byId.prefetch({ id });
+  // Make sure to return { props: { trpcState: helpers.dehydrate() } }
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+      id,
+    },
+  };
 }
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,4 @@
-import { auth, clerkClient } from "@clerk/nextjs";
+import { clerkClient } from "@clerk/nextjs";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import {
@@ -52,6 +52,18 @@ const ratelimit = new Ratelimit({
 });
 
 export const postsRouter = createTRPCRouter({
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const post = await ctx.prisma.post.findUnique({
+        where: { id: input.id },
+      });
+
+      if (!post) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return (await addUserDataToPosts([post]))[0];
+    }),
+
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
